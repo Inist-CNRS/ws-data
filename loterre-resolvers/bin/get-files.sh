01c466fc003a902b6b2a615b638f7e18f02cb098
François Parmentier <francois.parmentier@gmail.com>
2025-07-04 15:59:59 +0200

    refactor(get-files): streamline file downloads using get_url function
    Getting URL from lodex instance terminologies-liens.

--- loterre-resolvers/bin/get-files.sh
+++ loterre-resolvers/bin/get-files.sh
@@ -2,6 +2,20 @@
 
 set -euo pipefail
 
+get_url() {
+    vocab="$1"
+    value="${2:-SKOS}"
+    LODEX_URL="https://data.istex.fr/api/run/all-documents/?\$query\[uri\]=uid:/${vocab}-${value}"
+    HEADER="X-Lodex-Tenant: terminologies-liens"
+    BASE_URL=$(curl -H "$HEADER" "$LODEX_URL" | jq -r '.data[0].kTJq // empty')
+    if [[ -z "$BASE_URL" ]]; then
+        echo "Error: .data[0].kTJq not found in JSON response for vocab '$vocab'." >&2
+        exit 1
+    fi
+    URL="${BASE_URL}/download/${vocab}.xml"
+    echo "$URL"
+}
+
 printf "Downloading files...\n\n"
 
 printf "216\n"
@@ -11,34 +25,34 @@ printf "2XK - loterre-structures-recherche\n"
 curl --progress-bar -o ./data/2XK.skos http://mapping-tables.daf.intra.inist.fr/loterre-structures-recherche.xml
 
 printf "3JP\n"
-curl --progress-bar -o ./data/3JP.skos http://mapping-tables.daf.intra.inist.fr/3JP.skos
+curl --progress-bar -o ./data/3JP.skos "$(get_url 3JP)"
 
 printf "73G\n"
-curl --progress-bar -o ./data/73G.skos http://mapping-tables.daf.intra.inist.fr/73G.skos
+curl --progress-bar -o ./data/73G.skos "$(get_url 73G)"
 
 printf "9SD - loterre-pays\n"
-curl --progress-bar -o ./data/9SD.skos http://mapping-tables.daf.intra.inist.fr/loterre-pays.xml
+curl --progress-bar -o ./data/9SD.skos "$(get_url 9SD)"
 
 printf "BVM\n"
 curl --progress-bar -o ./data/BVM.skos http://mapping-tables.daf.intra.inist.fr/BVM.skos
 
 printf "D63 - loterre-communes\n"
-curl --progress-bar -o ./data/D63.skos http://mapping-tables.daf.intra.inist.fr/loterre-communes.xml
+curl --progress-bar -o ./data/D63.skos "$(get_url D63)"
 
 printf "JVR - loterre-mesh\n"
 curl --progress-bar -o ./data/JVR.skos http://mapping-tables.daf.intra.inist.fr/loterre-mesh.xml
 
 printf "MDL\n"
-curl --progress-bar -o ./data/MDL.skos http://mapping-tables.daf.intra.inist.fr/MDL.skos
+curl --progress-bar -o ./data/MDL.skos "$(get_url MDL)"
 
 printf "N9J\n"
 curl --progress-bar -o ./data/N9J.skos http://mapping-tables.daf.intra.inist.fr/N9J.skos
 
 printf "P21\n"
-curl --progress-bar -o ./data/P21.skos http://mapping-tables.daf.intra.inist.fr/P21.skos
+curl --progress-bar -o ./data/P21.skos "$(get_url P21)"
 
 printf "P66\n"
-curl --progress-bar -o ./data/P66.skos http://mapping-tables.daf.intra.inist.fr/P66.skos
+curl --progress-bar -o ./data/P66.skos "$(get_url P66)"
 
 printf "QX8\n"
-curl --progress-bar -o ./data/QX8.skos http://mapping-tables.daf.intra.inist.fr/QX8.skos
+curl --progress-bar -o ./data/QX8.skos "$(get_url QX8)"
